a2b440a9484ff4b4562a4b3ff8845befef75dbf2
IsaelCampos <programmerisael21@gmail.com>
2023-12-10 21:35:18 -0300

    Meu commit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Olá mundo")
+let nomeDoHeroi = "Isael"
+let nivelDoHeroi = 7000
+
+if(nivelDoHeroi <= 1000){
+    nivelDoHeroi = "Ferro"
+    console.log("O Herói de nome "+nomeDoHeroi+" está no nível de "+ nivelDoHeroi)
+}else if (nivelDoHeroi <= 2000){
+    nivelDoHeroi = "Bronze"
+    console.log("O Herói de nome "+nomeDoHeroi+" está no nível de "+ nivelDoHeroi)
+}else if (nivelDoHeroi <= 5000){
+    nivelDoHeroi = "Prata"
+    console.log("O Herói de nome "+nomeDoHeroi+" está no nível de "+ nivelDoHeroi)
+}else if (nivelDoHeroi <= 7000){
+    nivelDoHeroi = "Ouro"
+    console.log("O Herói de nome "+nomeDoHeroi+" está no nível de "+ nivelDoHeroi)
+}else if (nivelDoHeroi <= 8000){
+    nivelDoHeroi = "Platina"
+    console.log("O Herói de nome "+nomeDoHeroi+" está no nível de "+ nivelDoHeroi)
+}else if (nivelDoHeroi <= 9000) {
+    nivelDoHeroi = "Ascendene"
+    console.log("O Herói de nome "+nomeDoHeroi+" está no nível de "+ nivelDoHeroi)
+}else if (nivelDoHeroi <= 10000){
+    nivelDoHeroi = "Imortal"
+    console.log("O Herói de nome "+nomeDoHeroi+" está no nível de "+ nivelDoHeroi)
+}else {
+    nivelDoHeroi = "Radiante"
+    console.log("O Herói de nome "+nomeDoHeroi+" está no nível de "+ nivelDoHeroi)
+}
+
+
+
+
